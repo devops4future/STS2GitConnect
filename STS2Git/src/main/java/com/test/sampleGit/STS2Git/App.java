ab6755704071dba4a74002961ed707b7c12f6391
dhira <sharma.dhiraj27@gmail.com>
2022-08-24 14:51:41 +0530

    Test git commit

---
+++ STS2Git/src/main/java/com/test/sampleGit/STS2Git/App.java
@@ -0,0 +1,13 @@
+package com.test.sampleGit.STS2Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
